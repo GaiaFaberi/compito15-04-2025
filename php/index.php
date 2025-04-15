c9afeeafe18e37ec20781da5b8e060c58c10e0d8
lai2-24 <lai2-24@itismeucci.com>
2025-04-15 14:33:24 +0200

    ultime modifiche

--- php/index.php
+++ php/index.php
@@ -8,15 +8,15 @@ require __DIR__ . '/controllers/ScuoleController.php';
 
 $app = AppFactory::create();
 
-$app->get('/scuole/{scuola_id}/docenti', "DocentiController:index");
+$app->get('/scuole/{scuola_id:\d+}/docenti', "DocentiController:index");
 
-$app->get('/scuole/{idScuola}/docenti/{idDocente}', "DocentiController:show");
+$app->get('/scuole/{idScuola:\d+}/docenti/{idDocente:\d+}', "DocentiController:show");
 
-$app->post('/scuole/{id_scuola}/docenti', "DocentiController:create");
+$app->post('/scuole/{id_scuola:\d+}/docenti', "DocentiController:create");
 
-$app->put('/scuole/{id}/docenti/{idDoc}', "DocentiController:update");
+$app->put('/scuole/{id:\d+}/docenti/{idDoc:\d+}', "DocentiController:update");
 
-$app->delete('/scuole/{id}/docenti/{idDocen}', "DocentiController:destroy");
+$app->delete('/scuole/{id:\d+}/docenti/{idDocen:\d+}', "DocentiController:destroy");
 
 $app->get('/scuole', "ScuoleController:index");
 
@@ -27,14 +27,14 @@ $app->get('/scuole/{nome}', "ScuoleController:search");
 $app->post('/scuole', "ScuoleController:create");
 
 
-$app->put('/scuole/{id}', "ScuoleController:update");
+$app->put('/scuole/{id:\d+}', "ScuoleController:update");
 
 
-$app->delete('/scuole/{id}', "ScuoleController:destroy");
+$app->delete('/scuole/{id:\d+}', "ScuoleController:destroy");
 
 $app->get('/scuole/sortScuole/{column}[/{order}]', "ScuoleController:sort");
 
-$app->get('/scuole/sortDocenti/{idScuol}/docenti/{column}[/{order}]', "DocentiController:sort");
+$app->get('/scuole/sortDocenti/{idScuol:\d+}/docenti/{column}[/{order}]', "DocentiController:sort");
  
 
 $app->run();
